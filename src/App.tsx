9f644553aef5e3a63172c8fecab3a1fe84533e76
Md. Akif Zaman <akifzaman4040@gmail.com>
2026-01-19 20:36:26 +0600

    updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import Message from "./components/Message";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 //main App component
 function App() {
+  //immutable
+  const [drink, setDrink] = useState({
+    title: "Coke",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    //doing the next two lines will not render as expected because drink is immutable
+    // drink.price = 6;
+    // setDrink(drink);
+
+    //pass a new object to setDrink to update the state
+    // const newDrink = {
+    //   title: drink.title,
+    //   price: 6,
+    // };
+    // setDrink(newDrink);
+
+    //use the spread operator to copy the existing properties and update the one property that needs to change
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price}
+      <Button onClick={handleClick}>Show!</Button>
     </div>
   );
 }
