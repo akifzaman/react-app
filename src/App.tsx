958897011d90e6f4af970e64d15461791b366073
Md. Akif Zaman <akifzaman4040@gmail.com>
2026-01-20 19:26:03 +0600

    simplifying update logic with immer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import produce from "immer";
 
 //main App component
 function App() {
@@ -12,12 +13,22 @@ function App() {
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce(bugs, (draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
-      {bugs[0].fixed.toString()}
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <Button onClick={handleClick}>Show!</Button>
     </div>
   );
