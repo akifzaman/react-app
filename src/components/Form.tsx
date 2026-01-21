3c1e52d2fb50bed6d160d762814a8dfe0f8518e5
Md. Akif Zaman <akifzaman4040@gmail.com>
2026-01-21 19:58:24 +0600

    Applying validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,19 @@
 import React, { FormEvent, useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FieldValues, useForm } from "react-hook-form";
-const Form = () => {
-  const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data: FieldValues) => {
+interface FormData {
+  name: string;
+  age: number;
+}
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
     console.log(data);
   };
 
@@ -15,11 +24,29 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", {
+            required: true,
+            minLength: {
+              value: 3,
+              message: "Name must be at least 3 characters",
+            },
+            maxLength: {
+              value: 20,
+              message: "Name must be at most 20 characters",
+            },
+          })}
           type="text"
           className="form-control"
           id="name"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">Name field can not be empty</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            Name should be at least 3 characters long
+          </p>
+        )}
       </div>
 
       <div className="mb-3">
@@ -27,11 +54,24 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", {
+            required: true,
+            min: { value: 18, message: "Age must be at least 18" },
+            max: { value: 99, message: "Age must be at most 99" },
+          })}
           type="number"
           className="form-control"
           id="age"
         />
+        {errors.age?.type === "required" && (
+          <p className="text-danger">Age field can not be empty</p>
+        )}
+        {errors.age?.type === "min" && (
+          <p className="text-danger">Age should be at least 18</p>
+        )}
+        {errors.age?.type === "max" && (
+          <p className="text-danger">Age should be at most 99</p>
+        )}
       </div>
       {/* a button inside the form is used to submit the form */}
       <button className="btn btn-primary">Submit</button>
