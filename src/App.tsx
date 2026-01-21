457300c7ba4708310d0327d9fd4c195b036569e4
Md. Akif Zaman <akifzaman4040@gmail.com>
2026-01-21 14:29:48 +0600

    exercise: Updating state 3

--- src/App.tsx
+++ src/App.tsx
@@ -10,39 +10,38 @@ import { set } from "immer/dist/internal";
 //main App component
 function App() {
   //immutable
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["pepperoni"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1, price: 100 },
+      { id: 2, title: "Product 2", quantity: 1, price: 50 },
+    ],
   });
 
-  // const handleClick = () => {
-  //   //add new toppings without using immer
-  //   setPizza({
-  //     ...pizza,
-  //     toppings: [...pizza.toppings, "bacon", "salami"],
-  //   });
-  // };
-
   const handleClick = () => {
-    //add new toppings without using immer
-    setPizza(
-      produce(pizza, (draft) => {
-        draft.toppings.push("bacon");
-        draft.toppings.push("salami");
+    setCart(
+      produce(cart, (draft) => {
+        draft.items.map((item) => {
+          const foundItem = item.id === 1 ? item : null;
+          if (foundItem) {
+            foundItem.quantity = 2;
+          }
+        });
       })
     );
   };
 
   return (
     <div>
-      <p>Pizza Name: {pizza.name}</p>
-      <p>Pizza Toppings:</p>
+      <p>Items</p>
       <ul>
-        {pizza.toppings.map((topping) => (
-          <li key={topping}>{topping}</li>
+        {cart.items.map((item) => (
+          <li key={item.id}>
+            {item.title} {item.quantity}
+          </li>
         ))}
       </ul>
-      <Button onClick={handleClick}>Add New Toppings</Button>
+      <Button onClick={handleClick}>Add New Items</Button>
     </div>
   );
 }
