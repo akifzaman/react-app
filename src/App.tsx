2419bad2625117a64672e3c996af549de127650e
Md. Akif Zaman <akifzaman4040@gmail.com>
2026-01-21 14:00:00 +0600

    exercise: Updating state 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,39 @@ import { set } from "immer/dist/internal";
 //main App component
 function App() {
   //immutable
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-      age: 25,
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["pepperoni"],
   });
 
+  // const handleClick = () => {
+  //   //add new toppings without using immer
+  //   setPizza({
+  //     ...pizza,
+  //     toppings: [...pizza.toppings, "bacon", "salami"],
+  //   });
+  // };
+
   const handleClick = () => {
-    setGame(
-      produce(game, (draft) => {
-        draft.player.name = "Doe";
+    //add new toppings without using immer
+    setPizza(
+      produce(pizza, (draft) => {
+        draft.toppings.push("bacon");
+        draft.toppings.push("salami");
       })
     );
   };
 
   return (
     <div>
-      <p>Player ID: {game.id}</p>
-      <p>Player Name: {game.player.name}</p>
-      <p>Player Age: {game.player.age}</p>
-      <Button onClick={handleClick}>Update Player Name</Button>
+      <p>Pizza Name: {pizza.name}</p>
+      <p>Pizza Toppings:</p>
+      <ul>
+        {pizza.toppings.map((topping) => (
+          <li key={topping}>{topping}</li>
+        ))}
+      </ul>
+      <Button onClick={handleClick}>Add New Toppings</Button>
     </div>
   );
 }
