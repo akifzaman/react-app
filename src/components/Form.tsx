c487a2dc449aa0813410554247e3cc789aa6de97
Md. Akif Zaman <akifzaman4040@gmail.com>
2026-01-21 17:01:25 +0600

    accessing input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 const Form = () => {
+  //always initialize refs with null because initially there is no DOM element associated with them
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+
+  const person = { name: "", age: 0 };
+
+  const handSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Form submitted");
+    if (nameRef.current && ageRef.current) {
+      person.name = nameRef.current.value;
+      person.age = parseInt(ageRef.current.value);
+      console.log(person);
+    }
+  };
   return (
-    <form>
+    <form onSubmit={(event) => handSubmit(event)}>
       {/* div.mb-3>label.form-label+input.form-control */}
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" className="form-control" id="name" />
+        <input type="text" className="form-control" id="name" ref={nameRef} />
       </div>
 
       {/* div.mb-3>label.form-label+input[type=number].form-control */}
@@ -16,7 +31,7 @@ const Form = () => {
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input type="number" className="form-control" id="age" />
+        <input type="number" className="form-control" id="age" ref={ageRef} />
       </div>
 
       {/* button.btn.btn-primary */}
